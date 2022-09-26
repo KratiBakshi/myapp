68e327b4b9952a9463626a79d4439de0c10ddf30
kratibakshi <bakshi.krati.06@gmail.com>
2022-09-26 15:07:59 +0530

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp with git';
+  title = 'myapp with git with unew edited';
 }
